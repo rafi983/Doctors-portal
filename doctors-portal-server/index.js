f36f8c87bd28510458700deb894b5a249c03d440
rafi983 <rafiirfan211@gmail.com>
2021-11-22 02:43:06 +0600

    feature api done

---
+++ doctors-portal-server/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6kyz8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("banner_feature");
+    const featuresCollection = database.collection("features");
+
+    app.get("/features", async (req, res) => {
+      const cursor = featuresCollection.find({});
+      const features = await cursor.toArray();
+      res.send(features);
+    });
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("doctors portal server running");
+});
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
